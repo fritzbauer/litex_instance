02461fd555e738468d8ef843a36d8fdf41eb9b05
TrailingEdge <roubro1991@gmx.de>
2022-11-18 16:46:27 +0100

    some refactoring; still builds

--- instance_example.py
+++ instance_example.py
@@ -15,17 +15,18 @@ class InstanceExample(Elaboratable):
 
         m.submodules.pll = pll = IntelCycloneVClockDomainGenerator()
 
-        def get_all_resources(name):
-            resources = []
-            for number in itertools.count():
-                try:
-                    resources.append(platform.request(name, number))
-                except ResourceError:
-                    break
-            return resources
+        leds     = [res.o for res in get_all_resources(platform, "led")]
+        buttons  = [res.i for res in get_all_resources(platform, "button")]
+        self.blinkLED(m, platform, leds, buttons)
 
-        leds     = [res.o for res in get_all_resources("led")]
-        buttons  = [res.i for res in get_all_resources("button")]
+        # get the litex SoC
+        m.submodules.litex_soc = litex_soc = LiteXSoC()
+        m.d.comb += leds[0].eq(litex_soc.led)
+
+        return m
+
+
+    def blinkLED(self, m, platform, leds, buttons):        
 
         inverts  = [0 for _ in leds]
         for index, button in zip(itertools.cycle(range(len(inverts))), buttons):
@@ -35,13 +36,21 @@ class InstanceExample(Elaboratable):
         timer = Signal(range(int(clk_freq//2)), reset=int(clk_freq//2) - 1)
         flops = Signal(len(leds))
 
-        m.d.comb += Cat(leds).eq(flops ^ Cat(inverts))
+        m.d.comb += Cat(leds[1]).eq(flops ^ Cat(inverts))
         with m.If(timer == 0):
             m.d.sync += timer.eq(timer.reset)
             m.d.sync += flops.eq(~flops)
         with m.Else():
             m.d.sync += timer.eq(timer - 1)
 
-        m.submodules.litex_soc = litex_soc = LiteXSoC()
+        return
 
-        return m
+
+def get_all_resources(platform, name):
+            resources = []
+            for number in itertools.count():
+                try:
+                    resources.append(platform.request(name, number))
+                except ResourceError:
+                    break
+            return resources
--- litex_soc.py
+++ litex_soc.py
@@ -4,7 +4,75 @@ import os
 import subprocess
 import shutil
 from amaranth_boards.resources import SDRAMResource, UARTResource
-#from utils import request_bare
+
+class LiteXSoC(Elaboratable):
+    def __init__(self):
+        self.led = Signal()
+
+
+    def elaborate(self, platform) -> Module:
+        m = Module()
+
+        platform.add_file("VexRiscv.v", open("VexRiscv.v"))
+        platform.add_file("qmtech_5cefa2.v", open("qmtech_5cefa2.v"))
+        platform.add_file("qmtech_5cefa2_mem.init", open("qmtech_5cefa2_mem.init"))
+        platform.add_file("qmtech_5cefa2_rom.init", open("qmtech_5cefa2_rom.init"))
+        platform.add_file("qmtech_5cefa2_sram.init", open("qmtech_5cefa2_sram.init"))
+
+        sdram_resource = platform.request("sdram") #this line only synthesizes if connectors is an empty list
+        #sdram_resource = request_bare(platform, "sdram", 0) #this line synthesizes even if connectors is filled properly
+
+        uart_resource = platform.request("uart")
+
+        # Excerpt of module definition of litex_soc from qmtech_5cefa2.v:
+        # module qmtech_5cefa2 (
+        #    input  wire clk50,
+        #    output wire sdram_clock,
+        #    output reg  serial_tx,
+        #    input  wire serial_rx,
+        #    output wire [12:0] sdram_a,
+        #    output wire [1:0] sdram_ba,
+        #    output wire sdram_cs_n,
+        #    output wire sdram_cke,
+        #    output wire sdram_ras_n,
+        #    output wire sdram_cas_n,
+        #    output wire sdram_we_n,
+        #    inout  wire [15:0] sdram_dq,
+        #    output wire [1:0] sdram_dm,
+        #    output reg  user_led0
+        #);
+
+        #dq = Signal(16)
+        #m.d.comb += dq.eq(sdram_resource.dq.i)
+        #m.d.comb += sdram_resource.dq.o.eq(dq.o)
+        #dq = Tristate(sdram_resource.dq.o, sdram_resource.dq.oe, sdram_resource.dq.i)
+
+        m.submodules.litex_soc = Instance("qmtech_5cefa2",
+            i_clk105 = ClockSignal("soc"),
+            i_clk105_ram= ClockSignal("sdram"),
+            o_sdram_clock = sdram_resource.clk,
+            o_serial_tx=uart_resource.tx,
+            i_serial_rx=uart_resource.rx,
+            o_sdram_a=sdram_resource.a,
+            o_sdram_ba=sdram_resource.ba,
+            o_sdram_cs_n=sdram_resource.cs,
+            o_sdram_cke=sdram_resource.clk_en,
+            o_sdram_ras_n=sdram_resource.ras,
+            o_sdram_cas_n=sdram_resource.cas,
+            o_sdram_we_n=sdram_resource.we,
+            io_sdram_dq=sdram_resource.dq,
+            #o_sdram_dq_oe=sdram_resource.dq.oe,
+            #o_sdram_dq_o=sdram_resource.dq.o,
+            #i_sdram_i=sdram_resource.dq.i,
+            #i_sdram_oe=sdram_resource.dq.oe,
+            #o_sdram_o=sdram_resource.dq.o,
+            o_sdram_dm=sdram_resource.dqm,
+            o_user_led0=self.led
+        )
+
+
+        return m
+
 
 def request_bare(platform, name, number):
     """Request bare pins.
@@ -31,71 +99,3 @@ def request_bare(platform, name, number):
         dir={io.name: "-" for io in res.ios},
         xdr={io.name: 0 for io in res.ios},
     )
-
-class LiteXSoC(Elaboratable):
-    def __init__(self):
-        self.led = Signal()
-
-
-    def elaborate(self, platform) -> Module:
-        m = Module()
-
-        platform.add_file("VexRiscv.v", open("VexRiscv.v"))
-        platform.add_file("qmtech_5cefa2.v", open("qmtech_5cefa2.v"))
-        platform.add_file("qmtech_5cefa2_mem.init", open("qmtech_5cefa2_mem.init"))
-        platform.add_file("qmtech_5cefa2_rom.init", open("qmtech_5cefa2_rom.init"))
-        platform.add_file("qmtech_5cefa2_sram.init", open("qmtech_5cefa2_sram.init"))
-
-        #/ home / rouven / Computer / Coden / Audio / fpga / pythondata - cpu - vexriscv / pythondata_cpu_vexriscv / verilog / VexRiscv.v
-        #sdram_resource = (SDRAMResource)(platform.request("sdram"))
-        #uart_resource = (UARTResource())(platform.request("uart"))
-        sdram_resource = request_bare(platform, "sdram", 0)
-        uart_resource = platform.request("uart")
-
-        #module qmtech_5cefa2 (
-        #    input  wire clk50,
-        #    output wire sdram_clock,
-        #    output reg  serial_tx,
-        #    input  wire serial_rx,
-        #    output wire [12:0] sdram_a,
-        #    output wire [1:0] sdram_ba,
-        #    output wire sdram_cs_n,
-        #    output wire sdram_cke,
-        #    output wire sdram_ras_n,
-        #    output wire sdram_cas_n,
-        #    output wire sdram_we_n,
-        #    inout  wire [15:0] sdram_dq,
-        #    output wire [1:0] sdram_dm,
-        #    output reg  user_led0
-        #);
-
-        #dq = Signal(16)
-        #m.d.comb += dq.eq(sdram_resource.dq.i)
-        #m.d.comb += sdram_resource.dq.o.eq(dq.o)
-
-        #dq = Tristate(sdram_resource.dq.o, sdram_resource.dq.oe, sdram_resource.dq.i)
-        m.submodules.litex_soc = Instance("qmtech_5cefa2",
-            i_clk105 = ClockSignal("soc"),
-            i_clk105_ram= ClockSignal("sdram"),
-            o_sdram_clock = sdram_resource.clk,
-            o_serial_tx=uart_resource.tx,
-            i_serial_rx=uart_resource.rx,
-            o_sdram_a=sdram_resource.a,
-            o_sdram_ba=sdram_resource.ba,
-            o_sdram_cs_n=sdram_resource.cs,
-            o_sdram_cke=sdram_resource.clk_en,
-            o_sdram_ras_n=sdram_resource.ras,
-            o_sdram_cas_n=sdram_resource.cas,
-            o_sdram_we_n=sdram_resource.we,
-            io_sdram_dq=sdram_resource.dq,
-            #o_sdram_dq_oe=sdram_resource.dq.oe,
-            #o_sdram_dq_o=sdram_resource.dq.o,
-            #i_sdram_i=sdram_resource.dq.i,
-            #i_sdram_oe=sdram_resource.dq.oe,
-            #o_sdram_o=sdram_resource.dq.o,
-            o_sdram_dm=sdram_resource.dqm,
-            o_user_led0=self.led
-        )
-
-
-        return m
